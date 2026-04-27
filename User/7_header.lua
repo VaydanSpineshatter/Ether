221fbe50316d309ac17060f3bacb41e5ca9cd79a
vaydan <vayandim@proton.me>
2026-04-27 02:02:50 +0200

    Refactor header sorting function and dropdown creation

--- User/7_header.lua
+++ User/7_header.lua
@@ -1,19 +1,17 @@
-local D,F,_,C,_=unpack(select(2,...))
-
-local function OnHeaderSort(_,data)
+local D,F,_,C=unpack(select(2,...))
+local function OnHeader(self,index,data)
     local parent=C.ChildFrames[7]
     if not parent then return end
-    if data.index<=3 then
-        D.DB["CONFIG"][11]=data.index
-        parent.sort:SetText("Sort by: "..data.text)
-        F:Fire(1,data.index)
-    elseif data.index<=5 then
-        D.DB["CONFIG"][12]=data.index
-        parent.direction:SetText("Direction: "..data.text)
-        F:Fire(1,data.index)
+    if index<=3 then
+        D.DB["CONFIG"][11]=index
+        parent.sort:SetText("Sort by: "..data)
+        F:Fire(1,index)
+    elseif index<=5 then
+        D.DB["CONFIG"][12]=index
+        parent.direction:SetText("Direction: "..data)
+        F:Fire(1,index)
     end
 end
-
 function F:Header(index)
     local parent=C.ChildFrames[index]
     if parent.Created then return end
@@ -40,20 +38,22 @@ function F:Header(index)
         end)
         C.MainButtons[5][i]=btn
     end
-    local Config,Data={},{"GROUP","CLASS","ASSIGNEDROLE","BOTTOM","TOP"}
-    for key,name in ipairs(Data) do
-        table.insert(Config,{text=name,index=key})
+    local data,config={"GROUP","CLASS","ASSIGNEDROLE","BOTTOM","TOP"},{}
+    for _,v in ipairs(data) do
+        config[#config+1]=v
     end
-    local dropdown=F:CreateEtherDropdown(parent,130,"Select Method",Config,OnHeaderSort)
+    local dropdown=F:CreateEtherDropdown(parent,130,"Select Method",config,OnHeader)
     dropdown:SetPoint("CENTER")
     local sort=parent:CreateFontString(nil,"OVERLAY")
     parent.sort=sort
     sort:SetFontObject(C.EtherFont)
-    sort:SetText("Sort By: "..Data[DB["CONFIG"][11]])
+    sort:SetText("Sort By: "..data[DB["CONFIG"][11]])
     sort:SetPoint("BOTTOMLEFT",dropdown,"TOPLEFT",0,10)
     local direction=parent:CreateFontString(nil,"OVERLAY")
     parent.direction=direction
     direction:SetFontObject(C.EtherFont)
-    direction:SetText("Direction: "..Data[DB["CONFIG"][12]])
+    direction:SetText("Direction: "..data[DB["CONFIG"][12]])
     direction:SetPoint("BOTTOMLEFT",sort,"TOPLEFT",0,10)
+    C.MainButtons[5][1]:Disable()
+    C.MainButtons[5][2]:Disable()
 end
